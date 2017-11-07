69f22a666a19d6246ab2925e1bd7f412027b418a
Travis-ko <traviskothlow@gmail.com>
2017-11-07 04:05:51 +0000

    commit with finished ruby enumerations

--- filters.rb
+++ filters.rb
@@ -2,15 +2,30 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-  # Your code Here
+  @candidates.select { |hash| hash[:id] == id }
 end
 
 def experienced?(candidate)
-  # Your code Here
+  candidate[:years_of_experience] >= 2
 end
 
 def qualified_candidates(candidates)
-  # Your code Here
+    candidates.select { |hash|
+                          experienced?(hash) &&
+                          github_points(hash) &&
+                          of_age(hash) &&
+                          good_languages(hash)}
 end
 
-# More methods will go below
+def github_points(candidate)
+  candidate[:github_points] > 100
+end
+
+def of_age(candidate)
+  candidate[:age] > 17
+end
+
+def good_languages(candidate)
+  candidate[:languages].include?('Ruby') &&
+  candidate[:languages].include?('Python')
+end
